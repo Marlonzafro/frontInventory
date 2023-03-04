81a205abf9e564822ce2c35155393428abc5eb84
Marlonzafro <marlonzafro888@gmail.com>
2023-03-03 20:49:18 -0500

    cambios

--- src/app/pages/crear-producto/crear-producto.component.ts
+++ src/app/pages/crear-producto/crear-producto.component.ts
@@ -1,4 +1,6 @@
+import { PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-producto',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-producto.component.scss']
 })
 export class CrearProductoComponent {
+
+    tituloComponente: string = 'crear'
+    productoForm: FormGroup
+    
+
+    constructor(private fb: FormBuilder){
+        this.productoForm = this.fb.group ({
+
+            modelos: ['', Validators.required],
+            fabricante: ['', Validators.required],
+            precio: ['', Validators.required],
+            rutaImg: ['', Validators.required],
+            asgisnado_a: ['', Validators.required],
+            estado: ['', Validators.required],
+            //stok//
+            numeroDocumento: ['', Validators.required],
+
+
+        })
+    }
+
+    enviarInformacion(){
+        console.log(this.productoForm)
+    }
 
 }
